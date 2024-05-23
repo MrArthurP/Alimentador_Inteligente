693d0421e9e8dd24a705e6920074f80cb8e87c93
MrArthurP <arthurpassos16@gmail.com>
2024-05-23 14:42:25 -0300

    Arrumando Bugs e dependências necessárias
    -> Modificando o local da pasta Templates para aderir ao aplicativo principal de controle do alimentador.
    -> Adicionando o aplicativo 'controle' aos aplicativos existentes no servidor.
    -> Criando as funções 'index' e 'control_led' para enviar informações do servidor.

--- Alimentador_Inteligente/controle/views.py
+++ Alimentador_Inteligente/controle/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+# controle/views.py
+from django.shortcuts import render
+from django.http import HttpResponse
+import requests
+
+NODEMCU_IP = "192.168.50.1"
+
+def index(request):
+    return render(request, 'controle/index.html')
+
+def control_led(request):
+    if request.method == "POST":
+        action = request.POST.get("action")
+        if action == "ligar":
+            requests.get(f"http://{NODEMCU_IP}/led?action=ligar")
+        elif action == "desligar":
+            requests.get(f"http://{NODEMCU_IP}/led?action=desligar")
+    return HttpResponse("ok")
+
